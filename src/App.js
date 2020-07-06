53b3a4f3f16bcf1bc635a674e4af5b093f6d22b0
ajhdrummer <thedrew88@gmail.com>
2020-07-06 10:44:53 -0400

    d

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component() {
+  render(){
   return (
     <div>
+
     </div>
   );
+}
 }
 
 export default App;
