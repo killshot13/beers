1bf89ef47f816b31be5cbcf812be834d4ddc493d
vlee13 <vlee13@gmail.com>
2020-07-06 09:19:42 -0700

    second commit

--- src/AllBeers.js
+++ src/AllBeers.js
@@ -1,16 +1,35 @@
-import React, {Component} from 'react';
-import axios from 'axios'
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-class AllBeers extends Component{
-  render(){
-  return (
-    <div>
-    <Link to='/'>Home</Link>
-    
-    </div>
-  );
-}
+class AllBeers extends Component {
+  state = {
+    beers: [],
+  };
+
+  async componentDidMount() {
+    let res = await axios.get('https://ih-beers-api2.herokuapp.com/beers');
+    this.setState({
+      beers: res.data,
+    });
+  }
+
+  displayBeers = () => {
+    return this.state.beers.map((eachBeer) => {
+      return (
+        <Link to={`/beers/${eachBeer._id}`}>
+          <h1>{eachBeer.name}</h1>
+          <img src={eachBeer.image_url} />
+          <div>{eachBeer.tagline}</div>
+          <div>{eachBeer.contributed_by}</div>
+        </Link>
+      );
+    });
+  };
+
+  render() {
+    return <div>{this.displayBeers()}</div>;
+  }
 }
 
 export default AllBeers;
